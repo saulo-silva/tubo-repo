e2902070bdb92cd46b6c7ce5e9d610df55f8892f
Saulo Silva <saulo@s10design.com.br>
2023-04-03 11:36:51 -0300

    update repo

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth information about Next.js features and API. Saulo
+            Find in-depth information about Next.js features and API.
           </p>
         </a>
 
