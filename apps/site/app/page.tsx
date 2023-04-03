679f2a6dc59d7b80d9783deae26383f4298a2132
Saulo Silva <saulo@s10design.com.br>
2023-04-03 17:35:10 -0300

    [major] test upgrade

--- apps/site/app/page.tsx
+++ apps/site/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Updated Get started by editing&nbsp;
+          Updating Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
